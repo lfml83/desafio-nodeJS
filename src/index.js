d82de0fcdc08f8f83ecad6b553fac7d2f2ff56b5
Luiz Fernando Martins Leite <luizfernandoleite@gmail.com>
2021-05-06 17:10:48 -0300

    third and fourth test completed

--- src/index.js
+++ src/index.js
@@ -11,7 +11,18 @@ app.use(express.json());
  const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  
+  const {username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+  //verificando se o usuario existe
+  if(!user){
+    return response.status(404).json({error : "User not found!"});
+  }
+
+  request.user = user;
+
+  return next();
 
 }
 
@@ -42,10 +53,26 @@ app.post('/users', (request, response) => {
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
   // Complete aqui
+  const { user } = request;
+  return response.json(user.todos)
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const {user} = request;
+  const {title, deadline} = request.body;
+
+  const todo ={ 
+    id: uuidv4(), // precisa ser um uuid
+    title,
+    done: false, 
+    deadline: new Date(deadline), 
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo)
+
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
